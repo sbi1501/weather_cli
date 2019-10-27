97763de61a25ab70c5b574466b55daf6e7745959
Seraphim Bogomolov <seraphim.bogomolov@gmail.com>
2019-10-28 01:12:58 +0700

    add docstrings

--- weather.py
+++ weather.py
@@ -3,13 +3,22 @@ import requests
 import datetime
 
 def get_current_weather(latitude, longitude, api_key):
+    '''Return current weather by latitude and longitude.
+
+    Keyword arguments:
+    latitude -- latitude in string
+    longitude -- longitude in string
+    api_key -- API key for the Dark Sky API
+
+    Returns: dictionary
+    '''
     url = f'https://api.darksky.net/forecast/{api_key}/{latitude},{longitude}'
     response = requests.get(url)
     if response.status_code == 200:
         currently_weather = response.json()['currently']
         time = humanize_timestamp(currently_weather['time'])
         summary = currently_weather['summary']
-        temperature = format_fahrenheit_to_celsius(currently_weather['temperature'])
+        temperature = convert_fahrenheit_to_celsius(currently_weather['temperature'])
         return {
             'time': time,
             'summary': summary,
@@ -19,9 +28,23 @@ def get_current_weather(latitude, longitude, api_key):
         return {}
 
 def humanize_timestamp(timestamp):
+    '''Return timestamp to human-readable format.
+
+    Keyword arguments:
+    timestamp -- timestamp in integer
+
+    Returns: string
+    '''
     return datetime.datetime.fromtimestamp(timestamp).strftime('%d-%m-%Y %H:%M:%S')
 
-def format_fahrenheit_to_celsius(fahrenheit):
+def convert_fahrenheit_to_celsius(fahrenheit):
+    '''Convert temperature in fahrenheit to celsius.
+
+    Keyword arguments:
+    fahrenheit -- fahrenheit in integer
+
+    Returns: float
+    '''
     return 5.0/9 * (fahrenheit - 32)
 
 parser = argparse.ArgumentParser(description='A little weather tool by coordinate')
